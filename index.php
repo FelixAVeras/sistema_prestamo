fce81bd0d99fc7a75f4448dac07dfa90772c834c
Felix Ant. Carvajal Veras <rocki2345@hotmail.com>
2018-04-29 23:35:04 -0400

    Add chart to the dashboard

--- index.php
+++ index.php
@@ -24,11 +24,53 @@
                 <?php include('sidebar.php'); ?>
             </div>
             <div class="col-md-9 col-12">
-                
+                <h1 class="text-left">Resumen</h1>
+                <br>
+                <canvas id="myChart" width="1000" height="400"></canvas>
             </div>
         </div>
     </div>
 <?php include('footer.php'); ?>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js"></script>
+<script>
+var ctx = document.getElementById("myChart");
+var myChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ["Monto Prestado", "Moras y atrazos", "Clientes Saldados", "Monto Recaudado", "Clientes", "Transacciones"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+                'rgb(255, 206, 86)',
+                'rgb(75, 192, 192)',
+                'rgb(153, 102, 255)',
+                'rgb(255, 159, 64)'
+            ],
+            borderColor: [
+                'rgb(255,99,132)',
+                'rgb(54, 162, 235)',
+                'rgb(255, 206, 86)',
+                'rgb(75, 192, 192)',
+                'rgb(153, 102, 255)',
+                'rgb(255, 159, 64)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+</script>
 
 
 
