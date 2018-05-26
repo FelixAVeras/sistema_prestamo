a3bfd48b951f95af53c52d0664e70f39e5291b05
Felix Ant. Carvajal Veras <rocki2345@hotmail.com>
2018-05-26 18:23:40 -0400

    Woking on loans section

--- newLoan.php
+++ newLoan.php
@@ -13,9 +13,17 @@
                         <div class="col-12 col-md-8">
                             <div class="form-group">
                                 <label>Nombre del cliente</label>
-                                <select name="customerName" id="customerName" class="form-control">
-                                    <option value="">-- Seleccione Cliente --</option>
-                                </select>
+                                <?php
+                                    $sql = "SELECT * FROM clientes";
+                                    $result = mysqli_query($connection, $sql);
+
+                                    echo "<select name='customerName' id='customerName' class='form-control'>";
+                                    while($row = mysqli_fetch_array($result)) {
+                                        echo "<option value='".$row['clienteID']."'>".$row['nombre']."</option>";
+                                    }
+                                    echo "</select>";
+                                    
+                                ?>
                             </div>
                         </div>
                         <div class="col-12 col-md-4">
@@ -58,24 +66,32 @@
                                 <input type="date" class="form-control" name="fechaPago" id="fechaPago">
                             </div>
                         </div>
-                        <div class="col-12 col-md-3">
+                        <div class="col-12 col-md-6">
                             <div class="form-group">
-                                <label>Interes %</label>
-                                <select class="form-control" name="interesPercent" id="interesPercent">
-                                    <option value="">-- Selecione --</option>
-                                    <option value="5">5%</option>
-                                    <option value="10">10%</option>
-                                    <option value="15">15%</option>
-                                    <option value="20">20%</option>
-                                </select>
+                                <label>Interes % <a href="javascript:;" class="ml-5" data-toggle="popover" data-trigger="hover" data-placement="right" data-content="Si el interes que busca no esta en la lista? Eliga la opcion 'Seleccione' y haga click en el boton de 'Nuevo Prestamo'">Ayuda ?</a></label>
+                                <div class="row">
+                                    <div class="col-12 col-md-6">
+                                        <select class="form-control" name="interesPercent" id="interesPercent">
+                                            <option value="">-- Selecione --</option>
+                                            <option value="5">5%</option>
+                                            <option value="10">10%</option>
+                                            <option value="15">15%</option>
+                                            <option value="20">20%</option>
+                                        </select>
+                                        <input type="number" class="form-control mt-2 nuevoInteres" placeholder="Nuevo Interes">
+                                    </div>
+                                    <div class="col-12 col-md-6">
+                                        <button type="button" class="btn btn-block btn-secondary" id="btnNuevoInteres" onclick="otroInteres()">Nuevo Interes</button:button>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div class="form-group form-check">
+                    <div class="form-group form-check garanteCheck">
                         <input type="checkbox" class="form-check-input" id="exampleCheck1">
                         <label class="form-check-label" for="exampleCheck1">Requiere Garante</label>
                     </div>
-                    <div class="row">
+                    <div class="row" id="filaGarante">
                         <div class="col-12 col-md-3">
                             <div class="form-group">
                                 <label>Nombre Garante</label>
@@ -114,6 +130,7 @@
                             <th># Pago</th>
                             <th>Fecha de pago</th>
                             <th>Cantidad a pagar</th>
+                            <th>Interes</th>
                             <th>Cantidad a pagar + Interes</th>
                             <th>Total restante</th>
                         </tr>
@@ -131,9 +148,41 @@
         </div>
     </div>
 
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <h3 class="text-center" id="ModalMessage"></h3>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
 
 <?php include('footer.php'); ?>
 <script src="js/calculateLoan.js"></script>
+<script>
+    $(function () {
+        $('[data-toggle="popover"]').popover()
+    });
+
+    function otroInteres() {
+        var otro = document.getElementById('interesPercent').selectedIndex;
+        var opciones = document.getElementById('interesPercent').options;
+        
+        $('input.nuevoInteres').css('display', 'block');
+        $('select#interesPercent').prop('disabled', true);
+    }
+</script>
 
 
 
